cc8c4c466abdf32176a630f1c44fdff61e1fec3f
jidexl21 <32308886+jidexl21@users.noreply.github.com>
2020-06-23 19:59:00 +0100

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-
-
 #!/bin/bash
 
 interactive="-i"
@@ -11,7 +9,7 @@ while [ "$1" != "" ]; do
         -u | --unit )           shift
                                 unit="-$1"
                                 ;;
-        -s | --silent )    	silent=1
+        -s | --silent )         silent=1
                                 ;;
         -h | --help )           usage
                                 exit
@@ -21,22 +19,23 @@ while [ "$1" != "" ]; do
     esac
     shift
 done
-
+echo "./autodeploy$unit.cfg"
 CFG_FILE="./autodeploy$unit.cfg"
 if [ -f "$CFG_FILE" ]; then
     echo "deploying unit '$unit'"
-else 
+else
     echo "Configuration for unit '$unit' not found"
     exit 0
 fi
 
 if [ $silent -eq 1 ]
 then
-	interactive=""
+        interactive=""
 fi
 
-source "utils/check.sh" $interactive
-source "./autodeploy$unit.cfg"
+
+source "./utils/check.sh" $interactive
+source $CFG_FILE
 
 rm -rf $FOLDER
 git clone $REPO --branch $BRANCH --single-branch
@@ -46,35 +45,35 @@ echo "about to complete deployment on '$TARGET_DIR'"
 echo "document type: '$BUILD_TYPE'"
 
 case $BUILD_TYPE in
-		"react")
-		echo "commencing deployment for react application"
-		npm install
-		echo "done with npm install"
-		npm run build
+                "react")
+                echo "commencing deployment for react application"
+                npm install
+                echo "done with npm install"
+                npm run build:development
 
-		echo "done with npm run build"
-                ls $DEPLOYMENT_DIR/$FOLDER 
-		#mkdir -p $DEPLOYMENT_DIR/$FOLDER
-		cp -r build/* $DEPLOYMENT_DIR/$FOLDER
-		##cd $DEPLOYMENT_DIR/$FOLDER
-		#pwd
-		echo "done with deployment"
-		sudo service nginx stop
-		sudo service nginx start
-			;;
-		*)
-		echo "commencing deployment for dotnetcore application"
-		#dotnet run -c Release
-		#rm -rf $DEPLOYMENT_DIR/$FOLDER
-		#mkdir -p $DEPLOYMENT_DIR/$FOLDER
-		dotnet build
-		#Stop Services
-		sudo service nginx stop
-		echo "about to start service: $SERVICE_NAME"
-		sudo systemctl stop $SERVICE_NAME.service
-		dotnet publish -c Release --output  $DEPLOYMENT_DIR/$FOLDER
-		#Restart Services
-		sudo systemctl start $SERVICE_NAME.service
-		sudo service nginx start
+                echo "done with npm run build"
+                ls $DEPLOYMENT_DIR/$FOLDER
+                #mkdir -p $DEPLOYMENT_DIR/$FOLDER
+                cp -r build/* $DEPLOYMENT_DIR/$FOLDER
+                ##cd $DEPLOYMENT_DIR/$FOLDER
+                #pwd
+                echo "done with deployment"
+                #systemctl stop nginx
+                #systemctl start nginx
+                        ;;
+                *)
+                echo "commencing deployment for dotnetcore application"
+                #dotnet run -c Release
+                #rm -rf $DEPLOYMENT_DIR/$FOLDER
+                #mkdir -p $DEPLOYMENT_DIR/$FOLDER
+                dotnet build
+                #Stop Services
+                #eval "sudo systemctl stop nginx"
+                echo "about to start service: $SERVICE_NAME"
+                eval "sudo systemctl stop $SERVICE_NAME"
+                dotnet publish -c Release --output  $DEPLOYMENT_DIR/$FOLDER
+                #Restart Services
+                eval "sudo systemctl start $SERVICE_NAME"
+                #eval "sudo systemctl start nginx"
 
 esac
